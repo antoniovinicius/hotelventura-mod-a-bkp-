278d43995af47f957df0bf3aef80e5c1e835db32
Renan Ribeiro <renan.lagee@gmail.com>
2023-03-12 18:53:47 -0300

    infra

--- docker/mysql/scripts/last.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE hotelventura.tb_usuarios ADD hash varchar(256) NULL;
-ALTER TABLE hotelventura.tb_usuarios ADD salt varchar(256) NULL;
-ALTER TABLE hotelventura.tb_usuarios MODIFY COLUMN senha varchar(256) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL;
-
